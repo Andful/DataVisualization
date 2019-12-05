ab5061511f87990c3d46c8c3f3bf9f4b22022696
Andrea Nardi <buongiorno19972@gmail.com>
2019-12-05 16:31:51 +0100

    improved ui

--- js/index.js
+++ js/index.js
@@ -1,28 +1,56 @@
 import * as d3 from "d3";
+import './fullscreen.js';
 
-let svg = d3.select("svg");
-let width = +svg.attr("width");
-let height = +svg.attr("height");
-
-// Map and projection
-let projection = d3.geoMercator()
-    .center([5.2913, 52.1326])                // GPS of location to zoom on
-    .scale(4000)                       // This is like the zoom
-    .translate([ width/2, height/2 ])
+function get_bounding_box(geojson,projection) {
+    let result = {}
+    let points = geojson
+    .features
+    .flatMap(x => x
+        .geometry
+        .coordinates
+        .flatMap(y => y[0]
+            .map(k => projection(k))
+        )
+    )
+    result.x = Math.min(...(points).map(x => x[0]))
+    result.y = Math.min(...(points).map(x => x[1]))
+    result.width = Math.max(...(points).map(x => x[0])) - result.x
+    result.height = Math.max(...(points).map(x => x[1])) - result.y
+    result.center = {x:result.x + result.width/2, y:result.y + result.height/2}
+    return result;
+}
 
 async function draw()
 {
-    let netherlands = await d3.json("data/the-netherlands.geojson");
-    let stations = await d3.json("data/stations.json");
-    let rails = await d3.json("data/rails.json");
-    let trains = await d3.json("data/trains.json");
+    let svg = d3.select("svg");
+    let width = svg.node().clientWidth;
+    let height = svg.node().clientHeight;
 
-    const station_radius = 3
-    const train_radius = 3
+    let [netherlands, stations, rails, trains] = await Promise.all([
+        d3.json("data/the-netherlands.geojson"),
+        d3.json("data/stations.json"),
+        d3.json("data/rails.json"),
+        d3.json("data/trains.json")
+    ]);
+
+    let projection = d3.geoMercator()
+
+    let bbox = get_bounding_box(netherlands,projection)
+    projection = projection
+    .center(projection.invert([bbox.center.x,bbox.center.y]))
+    .translate([width/2, height/2])
+    .scale(projection.scale()*Math.min(width/bbox.width, height/bbox.height))
+
+        //.scale(100)                       // This is like the zoom
+        //.translate([ width/2, height/2 ])
 
     const zoom = d3.zoom()
-      .scaleExtent([1, 8])
+      .scaleExtent([1, 10])
+      .translateExtent([[0,0],[width,height]])
       .on('zoom', zoomed);
+
+    const station_radius = 1
+    const train_radius = 1
 
     svg.call(zoom);
 
@@ -56,17 +84,48 @@ async function draw()
 
     map
         .selectAll(".station")
-        .data(stations.payload.map(d => projection([d.lng, d.lat])))
+        .data(stations.payload)
         .enter()
         .append("circle")
         .attr("class","station")
         .attr("fill", "red")
-        .attr("cx", d => d[0])
-        .attr("cy", d => d[1])
-        .attr("r",`${station_radius}px`)
+        .attr("cx", d => projection([d.lng, d.lat])[0])
+        .attr("cy", d => projection([d.lng, d.lat])[1])
+        .attr("r",`${Math.exp(station_radius)}px`)
         .style("stroke", "none")
+        .on("mouseover", showTooltip)
+        .on("mousemove", showTooltip)
+        .on("mouseout", removeTooltip);
 
-    map
+    let tooltip = svg
+        .append("g")
+        .attr("opacity",1)
+
+    tooltip.append("rect")
+    tooltip
+        .append("text")
+        .attr("x",20)
+        .attr("y",0)
+        .style("pointer-events", "none")
+
+    function showTooltip(d,i) {
+        let transform = this.getCTM();
+        var position = svg.node().createSVGPoint();
+        let selected = d3.select(this)
+        position.x = selected.attr("cx");
+        position.y = selected.attr("cy");
+        position = position.matrixTransform(transform);
+        tooltip.attr("transform",`translate(${position.x},${position.y})`)
+        tooltip.select('text').text(d.namen.lang)
+        tooltip.attr("opacity",1)
+    }
+
+    function removeTooltip(d,i) {
+        tooltip.attr("opacity",0)
+    }
+
+
+    /*map
         .selectAll(".train")
         .data(trains.payload.treinen.map(d => projection([d.lng, d.lat])))
         .enter()
@@ -76,37 +135,17 @@ async function draw()
         .attr("cx", d => d[0])
         .attr("cy", d => d[1])
         .attr("r",`${train_radius}px`)
-        .style("stroke", "none")
-
-    navigator.geolocation.getCurrentPosition(showPosition, errorHandler);
-    function errorHandler(err) {
-       if(err.code == 1) {
-          alert("Error: Access is denied!");
-       } else if( err.code == 2) {
-          alert("Error: Position is unavailable!");
-       }
-    }
-    function showPosition(position) {
-        let xy = projection([position.coords.longitude, position.coords.latitude])
-        map
-            .append("circle")
-            .attr("id","your_location")
-            .attr("fill", "green")
-            .attr("cx", d => xy[0])
-            .attr("cy", d => xy[1])
-            .attr("r",`${train_radius}px`)
-            .style("stroke", "none")
-    }
+        .style("stroke", "none")*/
 
     function zoomed() {
         map.attr('transform', d3.event.transform);
 
-        map
+        /*map
             .selectAll(".train")
-            .attr("r", `${train_radius/d3.event.transform.k}px`)
+            .attr("r", `${Math.exp(train_radius/d3.event.transform.k)}px`)*/
         map
             .selectAll(".station")
-            .attr("r", `${station_radius/d3.event.transform.k}px`)
+            .attr("r", `${Math.exp(station_radius/d3.event.transform.k)}px`)
     }
 }
 
